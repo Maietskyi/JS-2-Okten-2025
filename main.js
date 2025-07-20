a0858282e142b00fa5d5614cc18b287cbee8a6a3
Maietskyi <damaietskyi@gmail.com>
2025-07-20 16:08:11 +0300

    HW 6

--- main.js
+++ main.js
@@ -72,6 +72,9 @@
 // – Є “брудна” стрінга let str = ‘ dirty string   ‘ . Почистити її від зайвих пробілів.
 //
 //
+// let str = ' dirty string   '
+// console.log(str)
+// console.log(str.trim())
 //
 //
 //
