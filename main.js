3f4e4475bcbbee52dbb9ec4e5283cd78c2d7c183
Maietskyi <damaietskyi@gmail.com>
2025-08-11 22:07:35 +0300

    HW 9

--- main.js
+++ main.js
@@ -34,13 +34,18 @@
 //
 // – Є масив:
 //
-//     [‘Main’,’Products’,’About us’,’Contacts’]
 //
 // Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
 //
+// let arr = ['Main', 'Products', 'About us', 'Contacts']
 //
-//
-//
+// const ul1 = document.createElement('ul');
+// for (let item of arr) {
+//     const lis = document.createElement('li');
+//     lis.innerHTML = item;
+//     ul1.appendChild(lis);
+// }
+// document.body.appendChild(ul1)
 //
 // #jeBqHV525U5
 //
