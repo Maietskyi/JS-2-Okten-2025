f46579d1f367026dcfd8e2be6a604ed64fb8bb90
Maietskyi <damaietskyi@gmail.com>
2025-08-11 22:24:38 +0300

    HW 9

--- main.js
+++ main.js
@@ -71,9 +71,22 @@
 //
 // Завдання робити через цикли.
 //
-//
-//
-//
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (let course of coursesAndDurationArray) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    p.innerHTML = `title - ${course.title}. Month duration -${course.monthDuration}`;
+    div.appendChild(p);
+    document.body.appendChild(div);
+}
 //
 // =========================
 //
