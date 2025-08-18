34128609a3a87ef7e99be330fb9700f1be4bae24
Maietskyi <damaietskyi@gmail.com>
2025-08-18 23:39:22 +0300

    HW 10

--- main.js
+++ main.js
@@ -140,12 +140,19 @@
 // document.body.appendChild(p);
 // #LhSfdhM3
 //
-// Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховище, в масив sessionsList зберігається інформація про дату та час відвідування сторінки. Є  сторінка sessionsListPage.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а малювати в DOM
+// Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховище, в масив sessionsList зберігається 
+// інформація про дату та час відвідування сторінки. Є  сторінка sessionsListPage.html (назва довільна), при 
+// відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в 
+// консоль, а малювати в DOM
 //
-//
-//
-//
-//
+const sessionsList = JSON.parse(localStorage.getItem('sessions')) || [];
+
+sessionsList.push(new Date());
+
+localStorage.setItem('sessions', JSON.stringify(sessionsList));
+
+
+
 //
 //
 // #Jg0gPO00
